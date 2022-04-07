3cef66a5b7da6a27064e600f6ae74d8fc520781e
Dan Hart <dhart94@gmail.com>
2022-04-07 19:26:32 -0400

    Initial Commit

---
+++ Her/HerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HerApp.swift
+//  Her
+//
+//  Created by Dan Hart on 4/7/22.
+//
+
+import SwiftUI
+
+@main
+struct HerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
